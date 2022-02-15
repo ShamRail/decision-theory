5d370262bc505bd2e64818b947dd389b7bb0e205
ShamRail <rail1999g102r@mail.ru>
2022-02-15 08:05:58 +0500

    Добавил чекстайл

--- backend/decision-theory/src/main/java/decision/theory/DecisionTheoryApplication.java
+++ backend/decision-theory/src/main/java/decision/theory/DecisionTheoryApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class DecisionTheoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DecisionTheoryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionTheoryApplication.class, args);
+    }
 
 }
