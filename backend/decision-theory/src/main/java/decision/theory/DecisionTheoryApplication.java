bc333aee5a26beed0fc5721034b1e25f05ea8897
ShamRail <rail1999g102r@mail.ru>
2022-02-15 07:15:42 +0500

    Init project

---
+++ backend/decision-theory/src/main/java/decision/theory/DecisionTheoryApplication.java
@@ -0,0 +1,13 @@
+package decision.theory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionTheoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionTheoryApplication.class, args);
+	}
+
+}
---
+++ backend/decision-theory/src/test/java/decision/theory/DecisionTheoryApplicationTests.java
@@ -0,0 +1,13 @@
+package decision.theory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionTheoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
