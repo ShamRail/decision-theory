9f5da8e1222a3428554d3fe7191a24630d710e99
ShamRail <rail1999g102r@mail.ru>
2022-03-20 19:44:22 +0500

    МБР: Добавлен вывод логов и тесты

--- backend/decision-theory/src/main/java/decision/theory/lab3/service/BraunRobinsonService.java
+++ backend/decision-theory/src/main/java/decision/theory/lab3/service/BraunRobinsonService.java
@@ -1,15 +1,18 @@
 package decision.theory.lab3.service;
 
-import decision.theory.lab3.interfaces.IBraunRobinsonService;
-import decision.theory.lab3.model.BraunRobinsonRow;
-import decision.theory.util.IRandomService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Service;
+
+import decision.theory.lab3.interfaces.IBraunRobinsonService;
+import decision.theory.lab3.model.BraunRobinsonRow;
+import decision.theory.util.IRandomService;
 
 @Service
 public class BraunRobinsonService implements IBraunRobinsonService {
@@ -19,9 +22,10 @@ public class BraunRobinsonService implements IBraunRobinsonService {
     public BraunRobinsonService(IRandomService randomService) {
         this.randomService = randomService;
     }
-    
+
     @Override
     public List<BraunRobinsonRow> calculate(double[][] matrix, int iterationAmount) {
+        Locale.setDefault(Locale.ENGLISH);
         if (matrix.length == 0 || matrix[0].length == 0) {
             throw new IllegalArgumentException("Matrix is empty or it's vector");
         }
@@ -31,35 +35,80 @@ public class BraunRobinsonService implements IBraunRobinsonService {
         var result = new ArrayList<BraunRobinsonRow>();
         var rowCount = matrix.length;
         var colCount = matrix[0].length;
-        var row = copyRow(matrix[(int) (randomService.generate(0, rowCount))]);
-        var col = copyCol(matrix, (int) (randomService.generate(0, colCount)));
+        var firstRow = (int) (randomService.generate(0, rowCount));
+        var firstCol = (int) (randomService.generate(0, colCount));
+        var row = copyRow(matrix[firstRow]);
+        var col = copyCol(matrix, firstCol);
         var min = findExtremum(col, (el1, el2) -> el1 < el2);
         var max = findExtremum(row, (el1, el2) -> el1 > el2);
-        var previousRow = new BraunRobinsonRow(1, 
+        var previousRow = new BraunRobinsonRow(1,
                 col, row, min.index + 1, min.value, max.index + 1, max.value
         );
         result.add(previousRow);
+        System.out.printf("Пусть при k = 1 i1 = %d, j1 = %d %n%n", firstCol + 1, firstRow + 1);
+        IntStream.range(0, colCount).forEach(i -> System.out.printf("g1%d = a%d%d = %.2f%n", i + 1, i + 1, firstCol + 1, col.get(i)));
+        System.out.println();
+        IntStream.range(0, rowCount).forEach(j -> System.out.printf("h1%d = a%d%d = %.2f%n", j + 1, firstRow + 1, j + 1, row.get(j)));
+        System.out.println();
+        System.out.printf(
+                "M1 = min {%s} = %.2f%n", col.stream().map(v -> String.format("%.2f", v)).collect(Collectors.joining(", ")), min.value
+        );
+        System.out.printf(
+                "V1 = max {%s} = %.2f%n", row.stream().map(v -> String.format("%.2f", v)).collect(Collectors.joining(", ")), max.value
+        );
+        System.out.println();
+        System.out.printf("i1 = %d, j1 = %d %n%n", min.index + 1, max.index + 1);
+
         for (int k = 2; k <= iterationAmount; k++) {
+            System.out.printf("k = %d%n%n", k);
             var colResult = new ArrayList<Double>(previousRow.getColResult());
             var rowResult = new ArrayList<Double>(previousRow.getRowResult());
             for (int i = 0; i < rowCount; i++) {
+                System.out.printf(
+                        "g_%d_%d = g_%d_%d + a%d%d = %.2f + %.2f = %.2f%n",
+                        k, i + 1, k - 1, i + 1, i + 1, max.index + 1, colResult.get(i),
+                        matrix[i][max.index], colResult.get(i) + matrix[i][max.index]
+                );
                 colResult.set(i, colResult.get(i) + matrix[i][max.index]);
             }
+            System.out.println();
+
             for (int j = 0; j < colCount; j++) {
+                System.out.printf(
+                        "h_%d_%d = h_%d_%d + a%d%d = %.2f + %.2f = %.2f%n",
+                        k, j + 1, k - 1, j + 1, min.index + 1, j + 1, rowResult.get(j),
+                        matrix[min.index][j], rowResult.get(j) + matrix[min.index][j]
+                );
                 rowResult.set(j, rowResult.get(j) + matrix[min.index][j]);
             }
+            System.out.println();
+
             min = findExtremum(colResult, (el1, el2) -> el1 < el2);
             max = findExtremum(rowResult, (el1, el2) -> el1 > el2);
             previousRow = new BraunRobinsonRow(
-                    k, colResult, rowResult, 
-                    min.index + 1, min.value / k, 
+                    k, colResult, rowResult,
+                    min.index + 1, min.value / k,
                     max.index + 1, max.value / k
             );
+
+            System.out.printf(
+                    "M%d = min {%s} / %d = %.2f / %d = %.2f%n",
+                    k, colResult.stream().map(v -> String.format("%.2f", v)).collect(Collectors.joining(", ")),
+                    k, min.value, k, min.value / k
+            );
+            System.out.printf(
+                    "V%d = max {%s} / %d = %.2f / %d = %.2f%n",
+                    k, rowResult.stream().map(v -> String.format("%.2f", v)).collect(Collectors.joining(", ")),
+                    k, max.value, k, max.value / k
+            );
+            System.out.println();
+            System.out.printf("i%d = %d, j%d = %d %n%n", k, min.index + 1, k, max.index + 1);
+
             result.add(previousRow);
         }
         return result;
     }
-    
+
     private Extremum findExtremum(List<Double> elements, BiPredicate<Double, Double> condition) {
         var extremum = new Extremum(elements.get(0), 0);
         for (var i = 1; i < elements.size(); i++) {
@@ -70,7 +119,7 @@ public class BraunRobinsonService implements IBraunRobinsonService {
         }
         return extremum;
     }
-    
+
     private Extremum maxInRow(double[] row) {
         var max = new Extremum(row[0], 0);
         for (var i = 1; i < row.length; i++) {
@@ -81,7 +130,7 @@ public class BraunRobinsonService implements IBraunRobinsonService {
         }
         return max;
     }
-    
+
     private Extremum minInCol(double[][] matrix, int col) {
         var min = new Extremum(matrix[0][col], 0);
         var colLength = matrix[col].length;
@@ -91,29 +140,29 @@ public class BraunRobinsonService implements IBraunRobinsonService {
                 min.index = j;
             }
         }
-        return min; 
+        return min;
     }
-    
+
     private List<Double> copyRow(double[] row) {
         return Arrays.stream(row).mapToObj(d -> d).collect(Collectors.toList());
     }
-    
+
     private List<Double> copyCol(double[][] matrix, int col) {
         var colLength = matrix[col].length;
         return IntStream.range(0, colLength).mapToObj(j -> matrix[j][col]).collect(Collectors.toList());
     }
-    
+
     private static class Extremum {
-        
+
         private double value;
-        
+
         private int index;
 
         Extremum(double value, int index) {
             this.value = value;
             this.index = index;
         }
-        
+
     }
-    
+
 }
--- backend/decision-theory/src/test/java/decision/theory/lab3/service/BraunRobinsonServiceTest.java
+++ backend/decision-theory/src/test/java/decision/theory/lab3/service/BraunRobinsonServiceTest.java
@@ -1,11 +1,14 @@
 package decision.theory.lab3.service;
 
-import decision.theory.lab3.model.BraunRobinsonRow;
-import decision.theory.util.RandomService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import decision.theory.lab3.model.BraunRobinsonRow;
+import decision.theory.util.RandomService;
 
 @SuppressWarnings("checkstyle:MagicNumber")
 public class BraunRobinsonServiceTest {
@@ -15,50 +18,93 @@ public class BraunRobinsonServiceTest {
      */
     @Test
     public void testCalculate() {
-        var matrix = new double[][] {
-                {6, 2, 5}, 
-                {4, 3, 7}, 
-                {5, 5, 6}
+        var matrix = new double[][]{
+            {6, 2, 5},
+            {4, 3, 7},
+            {5, 5, 6}
         };
         var iterationAmount = 3;
         var service = new BraunRobinsonService(getRandomStub(List.of(0.0, 0.0)));
         var expResult = List.of(
-               new BraunRobinsonRow(1, List.of(6.0, 4.0, 5.0), List.of(6.0, 2.0, 5.0), 2, 4, 1, 6),
-               new BraunRobinsonRow(2, List.of(12.0, 8.0, 10.0), List.of(10.0, 5.0, 12.0), 2, 4, 3, 6),
-               new BraunRobinsonRow(3, List.of(17.0, 15.0, 16.0), List.of(14.0, 8.0, 19.0), 2, 5, 3, 19.0 / 3.0)
+                new BraunRobinsonRow(1, List.of(6.0, 4.0, 5.0), List.of(6.0, 2.0, 5.0), 2, 4, 1, 6),
+                new BraunRobinsonRow(2, List.of(12.0, 8.0, 10.0), List.of(10.0, 5.0, 12.0), 2, 4, 3, 6),
+                new BraunRobinsonRow(3, List.of(17.0, 15.0, 16.0), List.of(14.0, 8.0, 19.0), 2, 5, 3, 19.0 / 3.0)
         );
         List<BraunRobinsonRow> result = service.calculate(matrix, iterationAmount);
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void testCalculate2() {
-        var matrix = new double[][] {
-                {3, 7, 9}, 
-                {6, 1, 0}, 
-                {2, 5, 10}
+        var matrix = new double[][]{
+            {3, 7, 9},
+            {6, 1, 0},
+            {2, 5, 10}
         };
         var iterationAmount = 2;
         var service = new BraunRobinsonService(getRandomStub(List.of(0.0, 1.0)));
         var expResult = List.of(
-               new BraunRobinsonRow(1, List.of(7.0, 1.0, 5.0), List.of(3.0, 7.0, 9.0), 2, 1, 3, 9),
-               new BraunRobinsonRow(2, List.of(16.0, 1.0, 15.0), List.of(9.0, 8.0, 9.0), 2, 0.5, 1, 4.5)
+                new BraunRobinsonRow(1, List.of(7.0, 1.0, 5.0), List.of(3.0, 7.0, 9.0), 2, 1, 3, 9),
+                new BraunRobinsonRow(2, List.of(16.0, 1.0, 15.0), List.of(9.0, 8.0, 9.0), 2, 0.5, 1, 4.5)
         );
         List<BraunRobinsonRow> result = service.calculate(matrix, iterationAmount);
         assertEquals(expResult, result);
     }
-    
+
+    @Test
+    public void testCalculate3() {
+        var matrix = new double[][]{
+            {10, 12, 2, 23},
+            {9, 56, 65, 29},
+            {14, 3, 37, 43},
+            {78, 4, 17, 28}
+        };
+        var iterationAmount = 3;
+        var service = new BraunRobinsonService(getRandomStub(List.of(0.0, 0.0)));
+
+        List<BraunRobinsonRow> result = service.calculate(matrix, iterationAmount);
+        for (var row : result) {
+            System.out.println(row);
+        }
+
+        var expResult = List.of(
+                new BraunRobinsonRow(1, List.of(10.0, 9.0, 14.0, 78.0), List.of(10.0, 12.0, 2.0, 23.0), 2, 9.0, 4, 23),
+                new BraunRobinsonRow(2, List.of(33.0, 38.0, 57.0, 106.0), List.of(19.0, 68.0, 67.0, 52.0), 1, 16.5, 2, 34),
+                new BraunRobinsonRow(3, List.of(45.0, 94.0, 60.0, 110.0), List.of(29.0, 80.0, 69.0, 75.0), 1, 15.0, 2, 80.0 / 3.0)
+        );
+
+        assertEquals(expResult, result);
+    }
+
+    @Disabled
+    @Test
+    public void testCalculate4() {
+        var matrix = new double[][]{
+            {3, 7, 9},
+            {6, 1, 0},
+            {2, 5, 10}
+        };
+        var iterationAmount = 3;
+        var service = new BraunRobinsonService(getRandomStub(List.of(1.0, 2.0)));
+        var expResult = List.of(
+                new BraunRobinsonRow(1, List.of(7.0, 1.0, 5.0), List.of(3.0, 7.0, 9.0), 2, 1, 3, 9),
+                new BraunRobinsonRow(2, List.of(16.0, 1.0, 15.0), List.of(9.0, 8.0, 9.0), 2, 0.5, 1, 4.5)
+        );
+        List<BraunRobinsonRow> result = service.calculate(matrix, iterationAmount);
+        assertEquals(expResult, result);
+    }
+
     private RandomService getRandomStub(List<Double> values) {
         return new RandomService() {
-            
+
             private int i = 0;
-            
+
             @Override
             public double generate(double minValue, double maxValue) {
                 return values.get(i++);
             }
-            
+
         };
     }
-    
+
 }
